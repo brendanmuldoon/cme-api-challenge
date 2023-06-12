ce7bf7330b1915e7452ad87d7f7c53b225d030bc
brendanmuldoon <brendymuldoon_2010@hotmail.co.uk>
2023-06-12 19:46:00 +0100

    Inital commit

---
+++ src/test/java/com/cme/palindromeapi/PalindromeApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cme.palindromeapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PalindromeApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
