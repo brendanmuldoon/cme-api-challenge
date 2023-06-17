deb43474427ad473ed4a18203440700b2af96634
brendanmuldoon <brendymuldoon_2010@hotmail.co.uk>
2023-06-17 16:02:17 +0100

    Add listener unit test

---
+++ src/test/java/com/cme/palindromeapi/listener/ActiveMQListenerImplTest.java
@@ -0,0 +1,33 @@
+package com.cme.palindromeapi.listener;
+
+import com.cme.palindromeapi.repository.PalindromeRepository;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.doNothing;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+
+@SpringBootTest
+class ActiveMQListenerImplTest {
+
+    @InjectMocks
+    private ActiveMQListenerImpl activeMQListener;
+
+    @Mock
+    private PalindromeRepository repository;
+
+    @Test
+    @DisplayName("Given a message, then call the repository write method once")
+    void testRetrieveMessageCallsRepo() {
+        String message = "test";
+        doNothing().when(repository).write(message);
+        activeMQListener.retrieveMessage(message);
+        verify(repository, times(1)).write(message);
+    }
+
+}
